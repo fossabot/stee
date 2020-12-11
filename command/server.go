efcb84ee363a6d4c54c7075a394d874abd6e0a14
Milan Rodriguez <me@milanr.org>
2020-12-11 21:54:24 +0100

    add better configuration handling with Viper

--- command/server.go
+++ command/server.go
@@ -1,73 +1,138 @@
 package command
 
 import (
+	"context"
+	"fmt"
 	"net/http"
-	"sync"
+	"os"
+	"os/signal"
 
 	steehttp "github.com/milanrodriguez/stee/http"
 	"github.com/milanrodriguez/stee/stee"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func init() {
 	rootCommand.AddCommand(serverCommand)
-  }
+}
   
-  var serverCommand = &cobra.Command{
+var serverCommand = &cobra.Command{
 	Use:   "server",
 	Short: "Start the Stee server",
 	Long:  `Start the Stee server`,
 	Run: ServerRun,
-  }
-  
+	Aliases: []string{"serve", "srv"},
+}
 
 // ServerConfig is the configuration for the Stee server
 type ServerConfig struct {
-	// Address on which the http server will listen. Defaults to ":8008".
-	ListenAddress string
-
-	// Prefix reserved for API paths. This string must end by a trailing slash. Defaults to "_api/". No leading slash required.
-	APIReservedURLPrefix string
-
-	// Flag to enable the web UI
-	EnableUI bool
-
-	// Prefix reserved for UI paths. This string must end by a trailing slash. Defaults to "_ui/". No leading slash required.
-	UIReservedURLPrefix string
+	Server struct { 
+	Address string
+	Port string
+	TLS struct {
+		Enabled bool
+		TLSCertPath string
+		TLSKeyPath string
+	}
+	API struct {
+		Enable bool
+		URLPathPrefix string
+		SimpleAPI struct {
+			Enable bool
+		}
+	}
+	UI struct {
+		Enable bool
+		URLPathPrefix string
+	}}
 }
 
 // ServerRun runs a Stee server. Blocking.
 func ServerRun(cmd *cobra.Command, args []string) {
-
 	// Create configuration
-
-	cfg := DefaultConfig()
-
-	// Start HTTP Listener
-
+	config := loadConfig().Server
+	// Create core
 	core := stee.NewCore()
-	handler := steehttp.RootHandler(core, cfg.APIReservedURLPrefix, cfg.UIReservedURLPrefix)
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func () {
-		defer wg.Done()
-		defer println("Stopped listing at http://" + cfg.ListenAddress)
-		http.ListenAndServe(cfg.ListenAddress, handler)
-	}()
-	println("Started listing at http://" + cfg.ListenAddress)
-	
-	wg.Wait()
-}
+	srv := steehttp.NewServer(steehttp.ServerConfig{
+		ListenAddress: config.Address + ":" + config.Port,
+		Handler: steehttp.RootHandler(core, config.API.URLPathPrefix, config.UI.URLPathPrefix),
+	})
 
-// DefaultConfig returns the default config for Stee.
-func DefaultConfig() ServerConfig {
-	config := ServerConfig{
-		ListenAddress: ":8008",
-		APIReservedURLPrefix: "/_api/",
-		EnableUI: true,
-		UIReservedURLPrefix: "/_ui/",
+	// Starting to listen
+	if config.TLS.Enabled {
+		go func() {
+			defer println("Stopped listening at https://" + srv.Addr)
+			println("Listening at https://" + srv.Addr)
+			err := srv.ListenAndServeTLS(config.TLS.TLSCertPath, config.TLS.TLSKeyPath)
+			if err != http.ErrServerClosed {
+				panic(fmt.Errorf("Server closed unexpectedly: %e", err))
+			}
+		}()
+	} else {
+		go func() {
+			defer println("Stopped listening at http://" + srv.Addr)
+			println("Listening at http://" + srv.Addr)
+			err := srv.ListenAndServe()
+			if err != http.ErrServerClosed {
+				panic(fmt.Errorf("Server closed unexpectedly: %e", err))
+			}
+		}()
 	}
 
-	return config
+	//////////////////////////////////////////////////////////////////
+	// At this point initialization is done, the server is running. //
+	// We're now listening for OS signals                           //
+	//////////////////////////////////////////////////////////////////
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	for range c {
+		fmt.Printf("\nInterruption requested. We're gonna perform a clean shutdown...\n")
+		srv.Shutdown(context.Background())
+		core.Close()
+		fmt.Printf("Bye!\n")
+		break
+	}
+	
+}
+
+// loadConfig loads the config from a file
+func loadConfig() ServerConfig {
+	// We're looking for a file named "stee.yaml"
+	viper.SetConfigName("stee")
+	viper.SetConfigType("yaml")
+
+	// In those directories
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./config/")
+	viper.AddConfigPath("/etc/stee/")
+
+	// Environement variables take precedence over file config. See https://github.com/spf13/viper#why-viper
+	viper.AutomaticEnv()
+
+	// Defaults are the less important values. https://github.com/spf13/viper#why-viper
+	setDefaults()
+
+	err := viper.ReadInConfig()
+	if err != nil {panic(fmt.Errorf("Fatal error config file: %s", err))}
+
+	var cfg ServerConfig
+	err = viper.Unmarshal(&cfg)
+	if err != nil {panic(fmt.Errorf("unable to decode into struct, %s", err))}
+
+	return cfg
+}
+
+// setDefaults sets the default config for Stee.
+func setDefaults() {
+	viper.SetDefault("server.address", "localhost")
+	viper.SetDefault("server.port", "8008")
+
+	viper.SetDefault("server.api.enable", true)
+	viper.SetDefault("server.api.ReservedURLPrefix", "_api")
+	viper.SetDefault("server.api.SimpleAPI.enable", true)
+
+	viper.SetDefault("server.ui.enable", true)
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,25 @@ module github.com/milanrodriguez/stee
 go 1.15
 
 require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20200217142428-fce0ec30dd00 // indirect
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/kr/text v0.2.0 // indirect
+	github.com/magiconair/properties v1.8.4 // indirect
+	github.com/mitchellh/mapstructure v1.4.0 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/smartystreets/assertions v1.2.0 // indirect
+	github.com/spf13/afero v1.5.1 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/cobra v1.1.1
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.6.1 // indirect
 	go.etcd.io/bbolt v1.3.5
-	golang.org/x/sys v0.0.0-20201207223542-d4d67f95c62d // indirect
+	golang.org/x/sys v0.0.0-20201211090839-8ad439b19e0f // indirect
+	golang.org/x/text v0.3.4 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/ini.v1 v1.62.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
